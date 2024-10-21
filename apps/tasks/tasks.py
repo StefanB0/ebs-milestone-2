43edb1085dbd128639b9752fb258ab7934ced87b
Boicu Stefan <stefanboicu1@gmail.com>
2024-10-21 14:53:58 +0300

    remove test functions

--- apps/tasks/tasks.py
+++ apps/tasks/tasks.py
@@ -1,22 +1,6 @@
 from celery import shared_task
 from django.core.mail import send_mail
 
-
-@shared_task
-def add(x, y):
-    return x + y
-
-
-@shared_task
-def mul(x, y):
-    return x * y
-
-
-@shared_task
-def xsum(numbers):
-    return sum(numbers)
-
-
 @shared_task(bind=True, max_retries=5, default_retry_delay=600)
 def c_send_mail(self, recipient, subject, message):
     try:
